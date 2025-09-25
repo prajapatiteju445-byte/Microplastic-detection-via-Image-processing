1a592eb6b9cce6c24c9a6b82317d9cb863216c62
Tejas prajapati <tech81186@gmail.com>
2025-09-25 15:19:12 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9352687062-bbb80",
+  "appId": "1:44691821934:web:15b21123d4aa75e7555d7f",
+  "apiKey": "AIzaSyA8v-Nx7FCXbMEgGdtSfIIP_r-H2zikaXI",
+  "authDomain": "studio-9352687062-bbb80.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "44691821934"
+};
